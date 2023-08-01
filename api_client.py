066a1a908e9bf15d89368dff5b90fa70325d208f
anikateagrawal <anikateag7316@gmail.com>
2023-08-01 10:07:05 +0530

    commit

---
+++ api_client.py
@@ -0,0 +1,47 @@
+import requests
+import json
+
+def call_flights_api():
+    flight = {
+        "flightNumber": "4123123",
+        "airline": {
+            "name": "Indigo"
+        },
+        "departureAirport": {
+            "name": "Delhi Airport",
+            "city": "Delhi"
+        },
+        "arrivalAirport": {
+            "name": "Bangalore Airport",
+            "city": "Bangalore"
+        }
+    }
+    response = requests.post("http://localhost:8080/flights", json = flight)
+    print(response.status_code)
+    
+    resp_json = json.loads(response.text)
+    print(resp_json)
+    flight_id = resp_json["id"]
+    seats = [
+        {
+            "number": "C1",
+            "status": "AVAILABLE"
+        },
+        {
+            "number": "C2",
+            "status": "AVAILABLE"
+        },
+        {
+            "number": "D1",
+            "status": "AVAILABLE"
+        },
+        {
+            "number": "D2",
+            "status": "AVAILABLE"
+        }
+    ]
+    response = requests.post("http://localhost:8080/flights/%s/seats" % flight_id, json=seats)
+    print(response.status_code)
+    print(response.text)
+
+call_flights_api()
